29d90bfdbd49b9e16ef931f6049968a1360685bc
Hit-Boy <sunguy2@mail.ru>
2021-03-15 21:03:20 +0300

    Rigidbody MovePosition

--- Gravity/Assets/Scripts/PlayerMovement.cs
+++ Gravity/Assets/Scripts/PlayerMovement.cs
@@ -4,60 +4,26 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController character;
+    Rigidbody PlayerRigidbody;
 
-    private int GravState = 1;
-    private float DirectionY = 0f;
-    private float DirectionX = 0f;
-    private float DirectionZ = 0f;
-    private bool CanJump = true;
-
-    public float SideSpeed = 8f;
-    public float ForwardSpeed = 10f;
+    public float Speed = 5f;
     public float Grav = 9.8f;
-    public float JumpSpeed = 10f;
 
-    // Start is called before the first frame update
     void Start()
     {
-        character = GetComponent<CharacterController>();
+        PlayerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        DirectionX = Input.GetAxis("Horizontal");
-        DirectionZ = Input.GetAxis("Vertical");
-
-        Vector3 Direction = new Vector3(DirectionX, DirectionY, DirectionZ);
-        if (character.isGrounded && CanJump == true)
-        {
-            if (Input.GetButtonDown("Jump") || Input.GetButton("Jump"))
-            {
-                DirectionY += JumpSpeed;
-                CanJump = false;
-            }
-        }
-
-        DirectionY -= Grav * Time.deltaTime;
-
-        if (character.isGrounded && CanJump == true)
-        {
-            DirectionY = 0f;    
-        }
-
-        Direction.y = DirectionY;
-        Direction.x *= SideSpeed;
-        Direction.z *= SideSpeed;
-
-        character.Move(Direction * Time.deltaTime);
-
     }
     void FixedUpdate()
     {
-        CanJump = true;
+        // PlayerRigidbody.AddForce();
+        Vector3 Direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+        Debug.Log(Direction);
+        PlayerRigidbody.MovePosition(transform.position + Direction *Time.deltaTime * Speed);
     }
-
 }
 
-    
