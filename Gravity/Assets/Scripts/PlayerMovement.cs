23732728349f7a63b93851ecf49bb2ed9b06dcae
Hit-Boy <sunguy2@mail.ru>
2021-03-15 16:44:30 +0300

    Character Controller
    fucked up collider

--- Gravity/Assets/Scripts/PlayerMovement.cs
+++ Gravity/Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     private CharacterController character;
 
-    private int GravState = 1;
+    private int GravState = 1; // 0 is left, 1 is middle, 2 is right
+    private int GravChange = 2; //1 is right, -1 is left, 2 is no change
     private float DirectionY = 0f;
     private float DirectionX = 0f;
     private float DirectionZ = 0f;
@@ -32,7 +33,7 @@ public class PlayerMovement : MonoBehaviour
         Vector3 Direction = new Vector3(DirectionX, DirectionY, DirectionZ);
         if (character.isGrounded && CanJump == true)
         {
-            if (Input.GetButtonDown("Jump") || Input.GetButton("Jump"))
+            if (Input.GetKeyDown("space") || Input.GetKey("space"))
             {
                 DirectionY += JumpSpeed;
                 CanJump = false;
@@ -50,6 +51,70 @@ public class PlayerMovement : MonoBehaviour
         Direction.x *= SideSpeed;
         Direction.z *= SideSpeed;
 
+        if (Input.GetKeyDown("q"))
+        {
+            GravChange = 0;
+            print(GravChange);
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            GravChange = 1;
+            print(GravChange);
+        }
+
+
+        switch (GravChange)
+        {
+            case 0:
+                switch (GravState)
+                {
+                    case 1: 
+                        transform.rotation = Quaternion.FromToRotation(Vector3.up, Vector3.right);
+                        GravState--;
+                        GravChange = 2;
+                        break;
+                    case 2:
+                        transform.rotation = Quaternion.FromToRotation(Vector3.up, Vector3.up);
+                        GravState--;
+                        GravChange = 2;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            case 1:
+                switch (GravState)
+                {
+                    case 0:
+                        transform.rotation = Quaternion.FromToRotation(Vector3.up, Vector3.up);
+                        GravState++;
+                        GravChange = 2;
+                        break;
+                    case 1:
+                        transform.rotation = Quaternion.FromToRotation(Vector3.up, Vector3.left);
+                        GravState++;
+                        GravChange = 2;
+                        break;
+                    default:
+                        break;
+                }
+                break;
+            default:
+                break;
+        }
+
+        switch (GravState)
+        {
+            case 0:
+                Direction = Quaternion.FromToRotation(Vector3.up, Vector3.right) * Direction;
+                break;
+            case 2:
+                Direction = Quaternion.FromToRotation(Vector3.up, Vector3.left) * Direction;
+                break;
+            default:
+                break;
+        }
         character.Move(Direction * Time.deltaTime);
 
     }
