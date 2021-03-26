bd50170632f37c45432b59b3553a82b6995b5e5b
Hit-Boy <sunguy2@mail.ru>
2021-03-26 18:19:41 +0300

    broken change of line

--- Gravity/Assets/Scripts/PlayerMovement.cs
+++ Gravity/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody PlayerRigidbody;
-    public static Quaternion targetRotation = Quaternion.identity;
+    
 
     [SerializeField]
     private float speed = 5f;
@@ -12,11 +12,17 @@ public class PlayerMovement : MonoBehaviour
     private float gravityForce = 9.8f;
     [SerializeField]
     private float jumpHeight = 3f;
+    [SerializeField]
+    private float changeLineSpeed = 0.5f;
+    [SerializeField]
+    private float changeLineTime = 0.2f;
 
+    private Quaternion targetRotation = Quaternion.identity;
+    private float changeOfLineDirection = 0f;
     private float playerHeight;
     private Vector3 gravityDirection = Vector3.down;
-    private float CurrentFloor = 0f;
     private bool jumpAvailability = true;
+    private bool changeLineAvailability = true;
     private Quaternion oldRotation = Quaternion.identity;
     private Quaternion rotateRight = Quaternion.Euler(0, 0, 90);
     private Quaternion rotateLeft = Quaternion.Euler(0, 0, -90);
@@ -29,17 +35,17 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
+        ChangeLineButtons();
         SwitchGravityCondition();
-        FindGravityDirection();
     }
     void FixedUpdate()
     {
-        Vector3 Direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
-        Direction = targetRotation * Direction;
+        Vector3 Direction = new Vector3(0, 0f, 1);
         PlayerRigidbody.MovePosition(transform.position +  Direction * Time.deltaTime * speed);
         PlayerRigidbody.AddForce(gravityDirection * gravityForce);
         Jump();
         jumpAvailability = true;
+        ChangeLine();
     }
 
     void Jump()
@@ -70,37 +76,72 @@ public class PlayerMovement : MonoBehaviour
     }
     void SwitchGravity(Quaternion actualRotation)
     {
-        targetRotation = actualRotation * targetRotation;
-        gravityDirection = targetRotation * Vector3.down;
-        transform.rotation = targetRotation * Quaternion.identity;
-        PlayerRigidbody.velocity = new Vector3(0f, 0f, PlayerRigidbody.velocity.z);
-        //  OldRotation = transform.rotation;
+        if (changeLineAvailability == true)
+        {
+            targetRotation = actualRotation * targetRotation;
+            gravityDirection = targetRotation * Vector3.down;
+            transform.rotation = targetRotation * Quaternion.identity;
+            PlayerRigidbody.velocity = new Vector3(0f, 0f, PlayerRigidbody.velocity.z);
+            //  OldRotation = transform.rotation;
+        }
     }
 
-    void FindGravityDirection()
+    void ChangeLineButtons()
     {
-        if (gravityDirection == Vector3.down)
+        if (changeLineAvailability == true)
         {
-            CurrentFloor = 0f;
+            if (Input.GetKeyDown("a"))
+            {
+                changeOfLineDirection = 1f;
+            }
+            else if (Input.GetKeyDown("d"))
+            {
+                changeOfLineDirection = 2f;
+            }
+            else
+                changeOfLineDirection = 0f;
         }
-        if (gravityDirection == Vector3.right)
+    }
+    void ChangeLine()
+    {
+        if (changeLineAvailability == true & changeOfLineDirection !=0f)
         {
-            CurrentFloor = 1f;
+            switch (changeOfLineDirection)
+            {
+                case 1:
+                    PlayerRigidbody.AddForce(rotateLeft * gravityDirection * changeLineSpeed, ForceMode.VelocityChange);
+                    changeLineAvailability = false;
+                    break;
+                case 2:
+                    PlayerRigidbody.AddForce(rotateRight * gravityDirection * changeLineSpeed, ForceMode.VelocityChange);
+                    changeLineAvailability = false;
+                    break;
+                default:
+                    break;
+            }
         }
-        if (gravityDirection == Vector3.up)
+
+        if (changeLineAvailability == false)
         {
-            CurrentFloor = 2f;
-        }
-        if (gravityDirection == Vector3.left)
-        {
-            CurrentFloor = 3f;
+            switch (changeOfLineDirection)
+            {
+                case 1:
+                    PlayerRigidbody.AddForce(rotateRight * gravityDirection * changeLineSpeed / changeLineTime);
+                    break;
+                case 2:
+                    PlayerRigidbody.AddForce(rotateLeft * gravityDirection * changeLineSpeed / changeLineTime);
+                    break;
+                default:
+                    break;
+            }
         }
     }
 
-    void ChangeLine()
+    void CheckBorderLine()
     { 
         
     }
+
 }
 
 
